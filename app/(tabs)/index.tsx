d7ba6d46b2a3c98a672bb3227c5f57cec8fa8b3a
codeTukai <tukaighosh753@gmail.com>
2026-05-10 13:43:56 +0530

    react native

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Tukai 🚀</Text>
+      <Text style={styles.subText}>My first React Native App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "blue",
+  },
+
+  subText: {
+    marginTop: 10,
+    fontSize: 18,
+  },
+});
